209cbf9a1f309fef24dcd813d91146295a6ce4ba
kjh0828u <kjh0828u@gmail.com>
2016-12-12 21:42:43 +0900

    로그인까지 구현

--- src/main/java/com/kjhapp/domain/User.java
+++ src/main/java/com/kjhapp/domain/User.java
@@ -1,6 +1,17 @@
-package com.kjhapp.web;
+package com.kjhapp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id	//프라이머리키
+	@GeneratedValue //자동증가
+	private Long id;
+	
+	@Column(nullable = false, length=20)
 	private String userId;
 	private String password;
 	private String name;
@@ -41,6 +52,13 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
+		
 	}
 	
 }
--- src/main/java/com/kjhapp/web/UserController.java
+++ src/main/java/com/kjhapp/web/UserController.java
@@ -1,29 +1,90 @@
 package com.kjhapp.web;
 
+import javax.servlet.http.HttpSession;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kjhapp.domain.User;
+import com.kjhapp.domain.UserRepository;
 
 @Controller
+@RequestMapping(value = "/users", produces = "application/mustache; charset=utf8")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
-	
-	@PostMapping("/create")
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping("")
 	public String create(User user) {
-		System.out.println(user );
-		users.add(user);
-		return "list";
+		System.out.println("user" + user);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
+	@GetMapping("")
+	public String list(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findOne(id));
+		return "/user/updateForm";
+	}
+
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/loginForm";
+	}
+
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		if (!password.equals(user.getPassword())) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
 	}
 	
-	@GetMapping("/list")
-	public String list(Model model){
-		model.addAttribute("users", users);
-		return "list";
+	@GetMapping("/logout")
+	public String logout(HttpSession session){
+		session.removeAttribute("user");
+		return "redirect:/";
 	}
+	
+	
+	
+	
+	
 	
 }
